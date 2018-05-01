2ff0ab3734281261369f53bac8d86bff051fd925
feiweiwei <feiweiwei@shrbank.com>
2018-05-01 18:01:57 +0800

    添加bus flex

---
+++ pages/bus/bus.js
@@ -0,0 +1,66 @@
+// pages/bus/bus.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    stopList: [{ "id": 0, "sid": null, "stopType": null, "stopId": "1.", "stopName": "施新路施湾二路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "2.", "stopName": "航城五路施湾二路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "3.", "stopName": "施湾三路航城五路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "4.", "stopName": "施新路川南奉公路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "5.", "stopName": "川南奉公路施新路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "6.", "stopName": "川南奉公路航城路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "7.", "stopName": "川南奉公路中圩路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "8.", "stopName": "川六公路川南奉公路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "9.", "stopName": "川六公路杜尹村" }, { "id": 0, "sid": null, "stopType": null, "stopId": "10.", "stopName": "川六公路（湾镇）" }, { "id": 0, "sid": null, "stopType": null, "stopId": "11.", "stopName": "川六公路（湾镇村）" }, { "id": 0, "sid": null, "stopType": null, "stopId": "12.", "stopName": "川六公路景园街" }, { "id": 0, "sid": null, "stopType": null, "stopId": "13.", "stopName": "川六公路六陈路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "14.", "stopName": "六陈路普迎路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "15.", "stopName": "川沙路六陈路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "16.", "stopName": "川沙路川宏路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "17.", "stopName": "川沙路川展路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "18.", "stopName": "川沙路迎宾大道" }, { "id": 0, "sid": null, "stopType": null, "stopId": "19.", "stopName": "川沙路川周公路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "20.", "stopName": "川沙路绣川路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "21.", "stopName": "川沙路翔川路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "22.", "stopName": "川沙路南桥路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "23.", "stopName": "川沙路新源路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "24.", "stopName": "川沙路新德路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "25.", "stopName": "川沙路华夏东路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "26.", "stopName": "川沙路王桥路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "27.", "stopName": "川沙路创新路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "28.", "stopName": "川沙路小白路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "29.", "stopName": "川沙路龙东大道" }, { "id": 0, "sid": null, "stopType": null, "stopId": "30.", "stopName": "川沙路胜利路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "31.", "stopName": "川沙路锦绣东路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "32.", "stopName": "川沙路景雅路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "33.", "stopName": "川沙路龚路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "34.", "stopName": "川沙路龚路支路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "35.", "stopName": "川沙路秦家港路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "36.", "stopName": "民雷路地铁站" }, { "id": 0, "sid": null, "stopType": null, "stopId": "37.", "stopName": "金海路民耀路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "38.", "stopName": "金海路金丰路" }, { "id": 0, "sid": null, "stopType": null, "stopId": "39.", "stopName": "金海路环东二大道" }, { "id": 0, "sid": null, "stopType": null, "stopId": "40.", "stopName": "金海路地铁站" }, { "id": 0, "sid": null, "stopType": null, "stopId": "41.", "stopName": "金海路金穗路" }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
